f9983889daf32a738d4322552ba3091309594239
ahmetcan <ahmetcanisik545@gmail.com>
2023-08-07 23:49:34 +0300

    login-signup system

--- src/supabase.ts
+++ src/supabase.ts
@@ -37,12 +37,25 @@ login.post("/signup", async (c) => {
   const { email, password } = await c.req.json();
   const supabase = createClient(c.env.SUPABASE_URL, c.env.SUPABASE_KEY);
 
+  const key = email.split("@")[0]; // TODO: save it to bucket
+  const url = `https://api.dicebear.com/6.x/bottts-neutral/svg?seed=${key}&backgroundColor=8e24aa,b6e3f4,c0aede,d81b60,ffd5dc&eyes=bulging,eva,frame1,frame2,happy,hearts,robocop,roundFrame01,roundFrame02,sensor,shade01&mouth=bite,diagram,grill03,smile01,square01,square02`
+  
   const { data, error } = await supabase.auth.signUp({
     email: email,
     password: password,
+    options: {
+      data: {
+        confirmation_sent_at: Date.now(),
+        avatar: url,
+      },
+    },
   });
 
-  return c.json({ data: data, error: error });
+  return c.json({
+    token: data.session?.access_token,
+    data: data,
+    error: error,
+  });
 });
 
 login.get("/twitter", async (c) => {
