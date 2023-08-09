1e1133a0f5a1f05883f430d2a270f7b81d8b26e5
ahmetcan <ahmetcanisik545@gmail.com>
2023-08-10 00:09:51 +0300

    OCR added. now we can convert image to text

--- src/image.ts
+++ src/image.ts
@@ -1,10 +1,65 @@
 import { Hono } from "hono";
+import { createClient } from "@supabase/supabase-js";
 
-type Bindings = {};
+type Bindings = {
+  SUPABASE_URL: string;
+  SUPABASE_KEY: string;
+};
 
 const image = new Hono<{ Bindings: Bindings }>();
 
 image.post("toText", async (c) => {
+  const supabase = createClient(c.env.SUPABASE_URL, c.env.SUPABASE_KEY);
+  const image = await c.req.blob(); // TODO: text olarak çekip direkt istek atılacak ocr
+  var name;
+
+  if (!!c.user) {
+    name = `${c.user.data.user.id}/${Date.now()}`;
+  } else {
+    name = `unknown/${Date.now()}${Math.random().toString().split(".")[1]}`;
+  }
+
+  const { data, error } = await supabase.storage
+    .from("images")
+    .upload(name, image);
+
+  if (error) {
+    return c.json({ error: error }, 400);
+  }
+
+  const url = supabase.storage.from("images").getPublicUrl(name).data.publicUrl;
+
+  const ocrData = await ocr(url).then((data) => {
+    // @ts-ignore
+    return data.ParsedResults[0].ParsedText;
+  });
+
+  return c.json({ text: ocrData, imageUrl: url });
+});
+
+const ocrUrl = "https://api.ocr.space/parse/image";
+const ocrKey = "helloworld";
+const ocrLanguage = "eng";
+
+const ocr = async (imgUrl: string) => {
+  const formData = new FormData();
+  formData.append("url", imgUrl);
+  formData.append("apikey", ocrKey);
+  formData.append("language", ocrLanguage);
+
+  const response = await fetch(ocrUrl, {
+    method: "POST",
+    headers: {
+      apikey: ocrKey,
+    },
+    body: formData,
+  });
+
+  const data = await response.json();
+  return data;
+};
+
+/* image.post("toText", async (c) => {
   const image = await c.req.blob();
   const img = (await image.text()).toString();
 
@@ -32,5 +87,5 @@ image.post("toText", async (c) => {
 
   //   return c.newResponse(image, 200, { "Content-Type": "image/png" });
 });
-
+ */
 export default image;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,41 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { createClient } from "@supabase/supabase-js";
 import user from "./supabase";
 import image from "./image";
+import authorization from "./authorization";
 
-const app = new Hono();
+type Bindings = {
+  SUPABASE_URL: string;
+  SUPABASE_KEY: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
 app.use("*", cors());
+
+app.use("/*", async (c, next) => {
+  const access_token = c.req.header("authorization");
+  const supabase = createClient(c.env.SUPABASE_URL, c.env.SUPABASE_KEY, {
+    global: { headers: { Authorization: `${access_token}` } },
+  });
+  const user = await supabase.auth.getUser();
+
+  if (user.error) {
+    console.log("not logged in");
+    return next();
+  }
+  // @ts-ignore
+  c.user = user;
+  console.log("logged in!!!");
+
+  return next();
+});
 
 app.get("/", (c) => c.json({ parve: "Welcome to Parve AI" }));
 
 app.route("/user", user);
 app.route("/image", image);
+app.route("/auth", authorization);
 
 export default app;
