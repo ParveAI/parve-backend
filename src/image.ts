a3f01e9b01191774236db1adf5bf9a3ab61fe067
cahmetcan <ahmetcanisik545@gmail.com>
2023-08-12 15:05:56 +0300

    shorten image pages code

--- src/image.ts
+++ src/image.ts
@@ -12,34 +12,32 @@ const image = new Hono<{ Bindings: Bindings }>();
 image.post("toText", async (c) => {
   const supabase = createClient(c.env.SUPABASE_URL, c.env.SUPABASE_KEY);
   const image = await c.req.blob(); // TODO: text olarak çekip direkt istek atılacak ocr
-  var userId;
-  var name;
+  var userId = null;
+  var name = `unknown/${Date.now()}${Math.random().toString().split(".")[1]}`;
 
   // @ts-ignore
   if (!!c.user) {
     // @ts-ignore
     userId = c.user.data.user.id;
     name = `${userId}/${Date.now()}`;
-  } else {
-    userId = null;
-    name = `unknown/${Date.now()}${Math.random().toString().split(".")[1]}`;
   }
-
   const { data, error } = await supabase.storage
     .from("images")
-    .upload(name, image);
+    .upload(name, image)
+    .then((data: any) => {
+      return data;
+    });
 
+  const supabaseUrl =
+    "https://ewbexihththlkcqybuoa.supabase.co/storage/v1/object/public/images/";
+  const url = supabaseUrl + data.path;
   if (error) {
     return c.json({ error: error }, 400);
   }
 
-  const url = supabase.storage.from("images").getPublicUrl(name).data.publicUrl;
-
   const ocrData = await ocr(url).then((data: any) => {
     return data.ParsedResults[0].ParsedText;
   });
-
-
 
   const { data: questionData, error: questionError } = await supabase
     .from("questions")
